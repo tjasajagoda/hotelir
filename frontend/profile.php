716b8e1e8499df9518ef9cb89faa3f6bcaf744b9
mpecnik <matej@pecnik.si>
2018-03-25 12:55:54 +0200

    index

--- frontend/profile.php
+++ frontend/profile.php
@@ -1,6 +1,6 @@
 <?php
   require("../api/checkCookie.php");
-  
+
   $dao = new MySQLDao();
   $uporabnisko_ime = $_COOKIE['hash'];
   $data = $dao->getUporabnik("'" . $uporabnisko_ime . "'");
@@ -71,7 +71,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="index.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>Hot</b></span>
       <!-- logo for regular state and mobile devices -->
@@ -86,203 +86,99 @@
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 4 messages</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li><!-- start message -->
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Support Team
-                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <!-- end message -->
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        AdminLTE Design Team
-                        <small><i class="fa fa-clock-o"></i> 2 hours</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Developers
-                        <small><i class="fa fa-clock-o"></i> Today</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Sales Department
-                        <small><i class="fa fa-clock-o"></i> Yesterday</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Reviewers
-                        <small><i class="fa fa-clock-o"></i> 2 days</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">See All Messages</a></li>
-            </ul>
-          </li>
           <!-- Notifications: style can be found in dropdown.less -->
           <li class="dropdown notifications-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="kosarica-st">
+              <i class="fa fa-shopping-basket"></i>
+
+              <script >
+                var storitve = JSON.parse(localStorage.getItem('seznamStoritev'));
+                var st = storitve.length;
+                 var element = $("#kosarica-st");
+                if(st>0){
+                  var template="<span class=\"label label-warning\" >"+st+"</span>";
+                  element.append(template);
+                }
+
+              </script>
             </a>
             <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
+              <li class="header">Košarica</li>
                   <li>
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
+                  <!-- inner menu: contains the actual data -->
+                    <ul class="menu" id="dropdown-kosarica">
+                      <li>
+                        <!-- inner menu: contains the actual data -->
+                        <ul class="menu" id="dropdown-kosarica">
+                          <script >
+                            $(document).ready(function(){
+                               $(function(){
+                                 objekt = {
+                                  "storitevId":1,
+                                  "kolicina":1
+                                };
+                                var sez = [];
+                                sez.push(objekt);
+                                objekt1 = {
+                                  "storitevId":2,
+                                  "kolicina":3
+                                };
+                                sez.push(objekt1);
+                                localStorage.setItem('seznamStoritev',JSON.stringify(sez));
+                                  $(".dropdown").on("show.bs.dropdown", function(event){
+                                    var element = $("#dropdown-kosarica");
+                                    element.empty();
+                                    var storitve = JSON.parse(localStorage.getItem('seznamStoritev'))
+
+
+                                    if(storitve == null){
+                                      var template =
+                                        "<li>\
+                                          <a href=\"#\">\
+                                          <i ></i>Košarica je prazna!\
+                                          </a>\
+                                        </li>";
+
+                                        element.append(template);
+                                    }else {
+                                      for (var i = 0; i < storitve.length; i++) {
+                                        //var idStoritve = storitve[i].storitevId;
+                                        var idStoritve="Vino"
+                                         var kolicina = storitve[i].kolicina;
+
+
+
+                                        var template =
+                                        "<li>\
+                                          <a>\
+                                            <div class=\"col-md-11\">\
+                                              <div>"+idStoritve+"</div>\
+                                              <div>Količina: "+kolicina+"</div>\
+                                            </div>\
+                                            <button class=\"fa fa-trash\" >\
+                                            </button>\
+                                          </a>\
+                                        </li>";
+
+
+                                        element.append(template);
+
+
+
+                                      }
+                                    }
+                                  });
+                               })
+                          });
+                          </script>
+                      </ul>
+                    </li>
+                  </ul>
                   </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                      page and may cause design problems
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-red"></i> 5 new members joined
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-user text-red"></i> You changed your username
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
-          </li>
+                 </ul>
+                </li>
           <!-- Tasks: style can be found in dropdown.less -->
-          <li class="dropdown tasks-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-flag-o"></i>
-              <span class="label label-danger">9</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 9 tasks</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li><!-- Task item -->
-                    <a href="#">
-                      <h3>
-                        Design some buttons
-                        <small class="pull-right">20%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">20% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
-                    <a href="#">
-                      <h3>
-                        Create a nice theme
-                        <small class="pull-right">40%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">40% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
-                    <a href="#">
-                      <h3>
-                        Some task I need to do
-                        <small class="pull-right">60%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">60% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
-                    <a href="#">
-                      <h3>
-                        Make beautiful transitions
-                        <small class="pull-right">80%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">80% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                </ul>
-              </li>
-              <li class="footer">
-                <a href="#">View all tasks</a>
-              </li>
-            </ul>
-          </li>
+
           <!-- User Account: style can be found in dropdown.less -->
           <!-- Control Sidebar Toggle Button -->
           <!-- <li>
@@ -295,18 +191,7 @@
               </li>
             </ul>
           </li> -->
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i class="fa fa-gears"></i>
-            </a>
-            <ul class="dropdown-menu">
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Profile</a></li>
-              <!-- <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li> -->
-              <li role="presentation" class="divider"></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Sign out</a></li>
-            </ul>
-          </li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Odjavi se</a></li>
         </ul>
       </div>
     </nav>
@@ -322,7 +207,7 @@
         </div>
         <div class="pull-left info">
           <p><?php echo $arr['ime'] . " " . $arr['priimek']; ?></p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          <a href="#"><i class="fa fa-circle text-success"></i> Dosegljiv</a>
         </div>
       </div>
       <!-- search form -->
@@ -367,25 +252,25 @@
           </ul>
         </li> -->
         <li >
-          <a href="#">
+          <a href="storitve.php">
             <i class="fa fa-shopping-basket"></i>
             <span>Storitve</span>
           </a>
         </li>
         <li >
-          <a href="#">
+          <a href="profile.php">
             <i class="fa fa-user"></i>
             <span>Podatki o uporabniku</span>
           </a>
         </li>
         <li >
-          <a href="#">
+          <a href="hotel.php">
             <i class="fa fa-building"></i>
             <span>O hotelu</span>
           </a>
         </li>
         <li >
-          <a href="#">
+          <a href="contact.php">
             <i class="fa fa-info"></i>
             <span>Kontakt</span>
           </a>
@@ -661,11 +546,11 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
+   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
+      <b>Skupina Hotelir (Startup Weekend)</b>
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2018</strong> All rights
     reserved.
   </footer>
 
